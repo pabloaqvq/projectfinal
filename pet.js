983231272e92309aa65073e806b67acb96fb8015
pabloaqvq <pabloaqvqs@hotmail.com>
2022-09-22 23:30:06 -0300

    asd

--- pet.js
+++ pet.js
@@ -1,42 +1,28 @@
-//final proj
-class Productos {
-
-
-    constructor(id, precio, nombre, tipo, stock, peso, img) {
-        this.id = id
-        this.precio = precio
-        this.nombre = nombre
-        this.tipo = tipo
-        this.stock = stock
-        this.peso = peso
-        this.img = img
-
-    }
-
-} const listaProductos = [];
-listaProductos.push(new Productos(1, "$6000", "Royal Eukanuba", "Alimento", "15", "20 kg", "./imgs/D_NQ_NP_708390-MLA44830126840_022021-O.jpg"));
-listaProductos.push(new Productos(2, "$3000", "Vital Premium", "Alimento", "15", "15kg", "./imgs/premium-adulto-vital-can-15-kg.jpg"));
-listaProductos.push(new Productos(3, "$4000", "Purina dogui", "Alimento", "23", "20kg", "./imgs/dogui-cachorros.jpg"));
-listaProductos.push(new Productos(4, "$500", "Ratita", "Juguete", "20", "", "./imgs/ratita.jpg"))
-listaProductos.push(new Productos(5, "$500", "Hueso", "Juguete", "3", "", "./imgs/descargar.jpg"))
-listaProductos.push(new Productos(6, "$400", "Pelota", "Juguete", "10", "", "imgs/images.jpg"))
-
-let carritoCompras = [];
 
 const contenedorProd = document.getElementById("cards")
 const contenedorCarrito = document.getElementById("carrito-contenedor")
+const vaciarCarro = document.getElementById("vaciar-carrito")
+
+let carritoCompras = [];
+
+
+vaciarCarro.addEventListener('click',() =>{
+    carritoCompras.length = 0;   
+    leerCarrito();
+   })
+
 
 //inyect card al dom
 listaProductos.forEach((producto) => {
     const div = document.createElement(`div`)
     div.classList.add("producto")
     div.innerHTML = `
-<img src=${producto.img} alt="">
+<img src=${producto.img} alt="img.jpg">
 <h3>${producto.nombre}</h3>
-<p>${producto.precio}</p>
+<p>$${producto.precio}</p>
 <p> ${producto.tipo}</p>
 <p>Stock: ${producto.stock}</p>
-<button id ="agregar${producto.id}" class="btn btn-primary">Agregar<i class="bi bi-cart-plus-fill"></i></button>`
+<button id ="agregar${producto.id}" class="btn btn-success">Agregar<i class="bi bi-cart-plus-fill"></i></button>`
     contenedorProd.appendChild(div)
 
     const boton = document.getElementById(`agregar${producto.id}`)
@@ -44,6 +30,10 @@ listaProductos.forEach((producto) => {
         agregarAlCarro(producto.id)
     })
 })
+
+
+
+
 //func abrir carrito
 document.getElementById("btn-car").addEventListener("click", () => {
     const container = document.getElementById("modalSheet")
@@ -55,33 +45,36 @@ document.getElementById("btn-car").addEventListener("click", () => {
         container.classList.remove("d-block")
         container.classList.add("d-none")
     })
-
 })
+
 //func agregar al carrito
 const agregarAlCarro = (agregarId) => {
     const item = listaProductos.find((agregar) => agregar.id === agregarId)
     carritoCompras.push(item)
     leerCarrito()
 }
+
+
+//delete carrito
+const eliminarCarrito = (prodId) => {
+    const item = carritoCompras.find((prod) => prod.id === prodId)
+    const indice = carritoCompras.indexOf(item)
+    carritoCompras.splice (indice,1)
+    leerCarrito()
+}      
 //func leer el carrito
 const leerCarrito = () => {
+    contenedorCarrito.innerHTML = ""
+
     carritoCompras.forEach((prod) => {
-        const div = document.createElement('div')
+        const div = document.createElement('tr')
         div.className = ("prodEnCarrito")
         div.innerHTML=`
-        <p>${prod.nombre}
-        <p>Precio: ${prod.precio}
-        <button onclick="eliminarDelCarrito (${prod.id})" class="btn btn-danger"><i class="bi bi-trash"></i></button>`
+        <td><b>${prod.nombre}</b></td>
+        <td>$${prod.precio}</td>
+        <td>Cantidad: <span id=cantidad>${prod.cantidad}</span></td>
+        <button onclick="eliminarCarrito(${prod.id})" class="btn btn-danger"><i class="bi bi-trash"></i></button>`
         contenedorCarrito.appendChild(div)
     })
 }
-/* //func vaciar carrito
-const vaciarCarrito = (delProd)=>{
-    const item = carritoCompras.find((prod => prod.id === delProd))
-    const indice = carritoCompras.indexOf(item)
-        carritoCompras.splice(indice, 1)
-        leerCarrito();
-} */
-
-
 
---
+++ petlist.js
@@ -0,0 +1,10 @@
+const listaProductos = [
+  { id: 1, precio: 6500, nombre: "Royal Eukanuba", tipo: "Alimento", cantidad: 1, stock: "x", img: "./imgs/D_NQ_NP_708390-MLA44830126840_022021-O.jpg" },
+  { id: 2, precio: 5000, nombre: "Vital Premium", tipo: "Alimento", cantidad: 1, stock: "x", img: ".//imgs/premium-adulto-vital-can-15-kg.jpg" },
+  { id: 3, precio: 4000, nombre: "Purina dogui", tipo: "Alimento", cantidad: 1, stock: "x", img: "./imgs/dogui-cachorros.jpg" },
+  { id: 4, precio: 4000, nombre: "Nutribon XQ", tipo: "Alimento", cantidad: 1, stock: "x", img: "imgs/nutribon-gato.jpg" },
+  { id: 5, precio: 120, nombre: "Vital Complete", tipo: "Alimento", cantidad: 1, stock: "x", img: "./imgs/vital-cat.jpg" },
+  { id: 6, precio: 120, nombre: "Ratita", tipo: "juguete", cantidad: 1, stock: "x", img: "./imgs/ratita.jpg" },
+  { id: 7, precio: 300, nombre: "Hueso", tipo: "juguete", cantidad: 1, stock: "x", img: "./imgs/descargar.jpg" },
+  { id: 8, precio: 200, nombre: "Pelota", tipo: "juguete", cantidad: 1, stock: "x", img: "imgs/images.jpg" }
+]
